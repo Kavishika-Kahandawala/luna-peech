3a193bec44093f871a48f87dcfc2044eb7e9f449
Kavishika-Kahandawala <lakshila0000@gmail.com>
2023-02-28 19:59:14 +0530

    coding product

---
+++ assets/js/products.js
@@ -0,0 +1,19 @@
+const products = new Map()
+products.set('item-cart-1', [
+  {
+    full_img_url: 'images/Products/Product 1/image-product-1.jpg',
+    thumb_img_url: 'images/Products/Product 1/image-product-1-thumbnail.jpg',
+  },
+  {
+    full_img_url: 'images/Products/Product 1/image-product-2.jpg',
+    thumb_img_url: 'images/Products/Product 1/image-product-2-thumbnail.jpg',
+  },
+  {
+    full_img_url: 'images/Products/Product 1/image-product-3.jpg',
+    thumb_img_url: 'images/Products/Product 1/image-product-3-thumbnail.jpg',
+  },
+  {
+    full_img_url: 'images/Products/Product 1/image-product-4.jpg',
+    thumb_img_url: 'images/Products/Product 1/image-product-4-thumbnail.jpg',
+  }
+])
